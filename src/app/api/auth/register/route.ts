8704b71bac122bab33956c7b31fcf60be9d7a57d
theprithwisingh <prithwisingh77@gmail.com>
2025-06-17 22:50:37 +0530

    Next-auth

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,37 +1,47 @@
 import { connectToDB } from "@/lib/db";
 import User from "@/models/User";
+import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
-
 
 export async function POST(request: NextRequest) {
     try {
-        const {email,password} = await request.json();
-        if (!email|| !password){
-            return NextResponse.json({error:"Email and password are required"},
-            {status:400}
-        )}
+        const { email, password } = await request.json();
+
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: "Email and password are required" },
+                { status: 400 }
+            );
+        }
+
         await connectToDB();
 
-        const existingUser = await User.findOne({email})
-        if (existingUser){
-            return NextResponse.json({error:"Email is already registered"},
-            {status:400}
-        )}
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return NextResponse.json(
+                { error: "Email is already registered" },
+                { status: 409 }
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         await User.create({
             email,
-            password
-        })
+            password: hashedPassword,
+        });
+
         return NextResponse.json(
-            {message:"User registered successfully"},
-            {status:201}
-        )
+            { message: "User registered successfully" },
+            { status: 201 }
+        );
 
     } catch (error) {
         console.error(error);
         return NextResponse.json(
-            {error:"failed to register user"},
-            {status:500}
-        )
+            { error: "Failed to register user" },
+            { status: 500 }
+        );
     }
 }
